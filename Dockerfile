227a1a4912216b49f05668d75f0214a231f42592
morroshub <lgmorrone@icloud.com>
2024-02-21 11:12:03 -0300

    se agrego la funcion curated que limpia el csv y carga su output dentro de un sql ademas de crear otro archivo csv con varias modificaciones mejorando la informacion del csv | documentacion hecha

--- Dockerfile
+++ Dockerfile
@@ -1,8 +1,21 @@
+# Creacion de la imagen 
+# 'docker build -t app:tag .'
+
+# Recarga del contenedor.
+# docker run -it -p puerto -v %PWD:/usr/src/apptag
+# docker run -it -p 8000:8000 -v %PWD:/usr/src/appnombre
+
+# Uso te derminal tty en docker como root 
+#docker exec -u root -t -i container_id /bin/bash
+
+# Combinando lineas de codigo
+#docker exec -u root -it -p 8000:8000 -v $(pwd):/usr/src/appnombre CONTAINER_ID /bin/bash
+
+
 FROM apache/airflow:2.8.1
 
 COPY requirements.txt /requirements.txt
 
-USER airflow
 
 RUN pip install --user --upgrade pip
 RUN pip install --no-cache-dir --user -r /requirements.txt
--- dags/dag_config.py
+++ dags/dag_config.py
@@ -1,3 +1,7 @@
+import os
+
+
+# Import Airflow
 from airflow import DAG
 from airflow.operators.bash_operator import BashOperator
 from airflow.operators.python_operator import PythonOperator
@@ -5,26 +9,110 @@ from airflow.providers.postgres.operators.postgres import PostgresOperator
 from airflow.sensors.sql_sensor import SqlSensor
 from airflow.utils.task_group import TaskGroup
 
+
+# Imports time
 from datetime import timedelta, datetime
+import pytz
+
+# Import pandas
 import pandas as pd
 from ydata_profiling import ProfileReport
-import pytz
+import matplotlib
+matplotlib.use('Agg')  # Use Agg backend (non-interactive) for saving plots
+
 
 
 # Activamos la imagen de docker para interactuar con un CLI de airflow
  # docker exec -it 'ID-Contenedor' bash
  # docker exec -it f7b806ca4b36 bash
 
+# Set date time and structure 
+TZ = pytz.timezone('America/Argentina/Buenos_Aires')
+TODAY = datetime.now(TZ).strftime('%Y-%m-%d')
+
 # Consumo de datos
 URL = 'https://raw.githubusercontent.com/ronnygang/webinar/main/titanic.csv'
 PATH = '/opt/airflow/dags/data/titanic.csv'
+OUTPUT_DQ = '/opt/airflow/dags/data/data_quality_report_{}.html'.format(TODAY)
+OUTPUT_SQL = '/opt/airflow/dags/sql/titanic_{}.sql'.format(TODAY)
+OUTPUT = '/opt/airflow/dags/data/titanic_curated_{}.csv'.format(TODAY)
+
 
 DEFAULT_ARGS = {
-    'owner': 'Morros',
+    'owner': 'Airflow',
     'retries' : 2,
     'retry_delay': timedelta(minutes=0.5)
 }
 
+# Funcion para crear el archivo mejorado html del CSV
+def _profilling():
+        # Configura MPLCONFIGDIR antes de utilizar pandas_profiling
+    os.environ['MPLCONFIGDIR'] = '/opt/airflow/dags/matplotlib_cache'
+
+    df = pd.read_csv(PATH)
+    profile = ProfileReport(df, title= 'Data Quality Report')
+
+    # Ruta de salida
+    profile.to_file(OUTPUT_DQ)
+
+
+def _curated():
+    # Lectura de archivo csv
+    df = pd.read_csv(PATH)
+
+    # Eliminamos columnas no necesarias
+    df.drop(['Ticket', 'Cabin'], axis=1, inplace=True)
+
+    # Rellenamos valores faltantes
+    df['Age'].fillna(df['Age'].median(), inplace=True)
+    df['Embarked'].fillna(df['Embarked'].mode()[0], inplace=True)
+
+    # Modificar la columna 'Name para obtener el nombre y apelido
+    df['Full Name'] = df['Name'].apply(lambda x: ''.join(x.split(',')[1].split('(')[0].strip().split()[1:]))
+
+    df['Full Name'] = df['Full Name'].str.replace('["\']', '', regex=True)
+
+    # Agregar una nueva columna 'Title' a partir de la columna 'Name'
+    df['Title'] = df ['Name'].apply(lambda x: x.split(',')[1].split('.')[0].strip())
+
+    # Elimno la columna 'Name'
+    df.drop(['Name'], axis=1, inplace=True)
+
+    # Simplificar los titulos
+    title_dict = {
+        'Capt' : 'Officer',
+        'Col' : 'Officer',
+        'Major' : 'Officer',
+        'Dr' : 'Officer',
+        'Rev' : 'Officer',
+        'Jonkheer' : 'Royalty',
+        'Don' : 'Royalty',
+        'Sir' : 'Royalty',
+        'the Countess' : 'Royalty',
+        'Lady' : 'Royalty',
+        'Ms' : 'Mrs',
+        'Mme' : 'Mrs',
+        'Mr' : 'Mr',
+        'Mlle' : 'Miss',
+        'Master' : 'Master',
+
+    }
+
+    # Mapeo de titulos
+    df['Title'] = df['Title'].map(title_dict)
+
+    # Reordenar las columnas en el orden deseado
+    df = df[['PassengerId', 'Full Name', 'Title', 'Survived', 'Pclass', 'Sex', 'Age', 'SibSp', 'Parch', 'Fare', 'Embarked']]
+
+    #Guardar el archivo procesado con todos los cambios en formato Csv
+    df.to_csv(OUTPUT, index=False)
+
+    # Iterar sobre las filas y crear los inserts
+    with open(OUTPUT_SQL, 'w') as f:
+        for index, row in df.iterrows():
+            values = f"({row['PassengerId']}, {row['Full Name']}, '{row['Title']}, {row['Survived']}, {row['Pclass']}, {row['Sex']}, {row['Age']}, {row['SibSp']}, {row['Parch']}, {row['Fare']},{row['Embarked']})" 
+            insert = f"INSERT INTO raw_titanic VALUES {values};\n"
+            f.write(insert)
 
 with DAG  (
     'dag_principal',
@@ -47,4 +135,26 @@ with DAG  (
 
 # Test dag para ahorrar recursos.
     #airflow task test dag + task 
-        # airflow tasks test dag_principal descargar_csv 2024-19-02
+        # airflow tasks test dag_principal descargar_csv 2024-02-19
+
+
+
+# Profiling del archivo descargado 
+    
+profiling = PythonOperator(
+    dag=dag,
+    task_id = 'profiling',
+    python_callable = _profilling,
+)
+
+# Test
+# airflow tasks test dag_principal profiling 2024-02-19
+
+curated = PythonOperator(
+    dag=dag,
+    task_id = 'curated',
+    python_callable = _curated,
+)
+
+# Test
+# airflow tasks test dag_principal curated 2024-02-19
